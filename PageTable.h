f7835b6409edc2a799ff1aae71137028151f25f7
Life Universe <riochr17@gmail.com>
2016-03-06 11:14:43 +0700

    Jenis data Page yang di simpan pada Shared Memory

--- PageTable.h
+++ PageTable.h
@@ -4,7 +4,7 @@ typedef struct {
     int Frame;
     int Dirty;
     int Requested;
-    } page_table_entry;
+    } page_table_entry; 
 
 typedef page_table_entry* page_table_pointer;
 //-----------------------------------------------------------------------------

b7d718d310ec2dc3538d7562a17104caa27a3643
Life Universe <riochr17@gmail.com>
2016-03-06 11:10:18 +0700

    Update OS.c

--- OS.c
+++ OS.c
@@ -13,6 +13,25 @@
 #include "PageTable.h"
 
 /*
+	R0 R2 R1 W3 R0 R2 R1 W4 R0 R2 R1 W3 R0 R2 R1 W4 R0 R2 R1 W3 R0 R2 R1 W4
+*/
+
+int banyakPage;
+int banyakFrame;
+page_table_pointer dataPage;
+int statusOS = 0;
+int aksesDisk = 0;
+
+//-----------------------------------------------------------------------------
+void PrintPageTable(page_table_entry PageTable[],int NumberOfPages) {
+    int Index;
+
+    for (Index =  0;Index < NumberOfPages;Index++) {
+        printf("%2d: Valid=%1d Frame=%2d Dirty=%1d Requested=%1d\n",Index, PageTable[Index].Valid,PageTable[Index].Frame,PageTable[Index].Dirty, PageTable[Index].Requested);
+    }
+}
+
+/*
 	Sinopsis
 		int alokasiSharedMemory(key_t kunci, int ukuran);
 
@@ -20,23 +39,33 @@
 		Jika alokasi berhasil, dikembalikan id shared memory, jika gagal -1.
 */
 int alokasiSharedMemory(key_t kunci, int ukuran){
+	// ID Shared Memory yang akan dialokasi
     int shmid;
-    char *data;
-    int mode;
 
-    /* connect to (and possibly create) the segment: */
+    // Melakukan alokasi Shared Memori jika memungkinkan dan mengubungkannya
     if ((shmid = shmget(kunci, ukuran, 0644 | IPC_CREAT)) == -1) {
         perror("shmget");
         return -1;
     }
 
-    //printf("shmid = %d | PID = %d\n", shmid, getpid());
-
-    /* attach to the segment to get a pointer to it: */
-    data = shmat(shmid, (void *)0, 0);
-    if (data == (char *)(-1)) {
+    // Menempatkan (attaching) Shared Memori yang telah dialokasi dan 
+    // dihubungkan diatas pada memori (RAM) dan alamat memori disimpan 
+    // pada variabel dataPage.
+    dataPage = (page_table_pointer) shmat(shmid, NULL, 0);
+    if (dataPage == (page_table_pointer)(-1)) {
         perror("shmat");
         return -1;
+    }
+
+    printf("The shared memory key (PID) is %d\n", kunci);
+
+    // Inisialisasi nilai awal page
+    printf("Initialized page table\n\n");
+    for(int i = 0; i < banyakPage; i++){
+    	dataPage[i].Valid = 0;
+    	dataPage[i].Frame = -1;
+    	dataPage[i].Dirty = 0;
+    	dataPage[i].Requested = 0;
     }
 
     return shmid;
@@ -52,13 +81,15 @@ int alokasiSharedMemory(key_t kunci, int ukuran){
 */
 int dealokasiSharedMemory(int kunci){
 
-    /* attach to the segment to get a pointer to it: */
-    page_table_pointer data = (page_table_pointer) shmat(kunci, NULL, 0);
-    if (shmdt(data) == -1){
+    // Melakukan detaching (bahasa indonesianya : pelepasan?) Shared Memori 
+    // dari memori (RAM).
+    // Setelah dilepas, Shared Memori masih bisa di tempatkan (attach) kembali
+    if (shmdt(dataPage) == -1){
         perror("shmdt");
         return -1;
     }
 
+    // Menghapus/dealokasi (deallocate/remove) Shared Memori yang dialokasi tadi.
     if(shmctl(kunci, IPC_RMID, NULL) == -1){
         perror("shmctl");
         return -1;
@@ -68,151 +99,182 @@ int dealokasiSharedMemory(int kunci){
 }
 
 /*
+	Sinopsis
+		int cariRequest();
+
+	Nilai Kembali
+		Jika terdapat request page dari MMU, maka dikembalikan nomor request,
+		-1 jika tidak ada.
+		Nilai kembali -1, artinya OS selesai bekerja
+*/
+int cariRequest(){
+	int i;
+	for(i = 0; i < banyakPage; i++)
+		//printf("dataPage[%d] = %d\n", i, dataPage[i].Requested);
+		if(dataPage[i].Requested != 0)
+			return i;
+
+	return -1;
+}
+
+/*
+	**PENTING**
+
+	FUNGSI UTAMA
+	Penanganan Memori dilakukan disini...
+*/
+void tanganiData(int nomor_request){
+
+	// Contoh...
+	printf("Process %d has requested page %d\n",dataPage[nomor_request].Requested, nomor_request);
+	printf("... lakukan sesuatu ...\n");
+	//# 3. Jika terdapat frame yang tidak ditempati, alokasikan.
+	// .. code ..
+
+	//# 4. Jika tidak ada seluruh frame ditempati, pilih page ‘victim’ yang akan di swap.
+	// .. code ..
+
+	//	   	+ Jika page ‘victim’ tersebut memiliki dirty bernilai true simulasikan penulisan pada disk, lakukan 
+	//		  sleep(1) dan tambahkan nilai pengaksesan pada disk.
+	//        .. code ..
+
+	//		+ Update page table untuk menyatakan page tersebut sudah tidak berada pada memori fisik.
+	//        .. code ..
+
+	//# 5. Simulasikan pemuatan page dengan sleep(1) dan tambahkan nilai pengaksesan pada disk.
+	// .. code ..
+
+	//# 6. Update page table untuk menyatakan page tersebut telah di load ke memori fisik dalam frame tersebut,
+	//     tentu dengan dirty bernilai false, dan kembalikan nilai Requested ke 0.
+	dataPage[nomor_request].Requested = 0;
+
+	//# 7. Cetak page table setelah diupdate.
+	// .. code ..
+}
+
+/*
 	Menangani sinyal SIGUSR1 yang dikirim oleh MMU
 */
-void  penangananSignal(int sig){
+void penangananSIGUSR1(int sig){
 
 	// tangani sinyal disini...
-    
-    signal(SIGUSR1, penangananSignal);
+	int no_request;
+
+	//# 1. Scan pada page table untuk menemukan Requested field yang diisi oleh MMU. 
+	no_request = cariRequest();
+	statusOS = no_request;
+
+	//# 2. Jika ditemukan, artinya page tersebut dibutuhkan oleh MMU. 
+	if(no_request != -1){
+		int PIDMMU = dataPage[no_request].Requested;
+
+		//# 3, 4, 5, 6, 7
+		tanganiData(no_request);
+
+		//# 8. Mengirimkan sinyal SIGCONT ke MMU untuk menyatakan page telah dimuat.
+		// kirim sinyal SIGCONT ke MMU
+		printf("Unblock MMU\n\n");
+		if (kill(PIDMMU, SIGCONT) == -1){
+            perror("Gagal mengirim sinyal SIGCONT ke MMU");
+            statusOS = -1;
+        }
+     	
+		// Menerima signal realtime selama simulator OS masih berjalan (online)
+		signal(SIGUSR1, penangananSIGUSR1);   
+	}
+
+	//printf("> Status OS : %s\n\n", (statusOS != -1 ? "Online" : "Offline"));
+}
+
+/*
+	Menangani sinyal interrup Ctrl-C yang dilakukan user pada simulator OS
+*/
+void penangananCtrlC(int sig){
+	char  c;
+
+	signal(sig, SIG_IGN);
+	printf("\nAnda menekan Ctrl-C? Ingin Berhenti? [y/t] : ");
+	c = getchar();
+	if (c == 'y' || c == 'Y')
+		statusOS = -1;
+	else
+		signal(SIGINT, penangananCtrlC);
 }
 
 int main(int argc, char *argv[]){
 
-	if(argc < 2){
-		printf("Error: argumen harus diisi\n");
+	if(argc < 3){
+		printf("> Kesalahan: argumen Page(arg1) dan Frame(arg2) tidak boleh kosong\n");
+		printf("> Gunakan perintah:\n\n");
+		printf("    %s <banyak page> <banyak frame>\n\n", argv[0]);
+		printf("    contoh: ");
+		printf("%s 5 2\n\n", argv[0]);
 		return 0;
 	}
 
-	int NumberOfPages = atoi(argv[1]);
+	banyakPage = atoi(argv[1]);
+	banyakFrame = atoi(argv[2]);
 
-	int idSharedMemory = alokasiSharedMemory(getpid(), NumberOfPages * sizeof(page_table_entry));
+	/*
+	printf("> Alokasi Shared Memory :\n");
+	printf("  Page  : %d\n", banyakPage);
+	printf("  Frame : %d\n", banyakFrame);
+	printf("\n");
+	printf("> Status :\n");
+	*/
+	int idSharedMemory = alokasiSharedMemory(getpid(), banyakPage * sizeof(page_table_entry));
 	if(idSharedMemory == -1){
 		printf("Alokasi Shared Memory Gagal\n");
 		printf("Program Berhenti.\n");
 		return 0;
 	}else{
-		printf("Alokasi Shared Memory berhasil | idSharedMemory = %d\n", idSharedMemory);
+		/*
+		printf("  Alokasi berhasil!\n");
+		printf("+----------------------+---------------+\n");
+		printf("| Shared Memory Key    | %13d |\n", getpid());
+		printf("+----------------------+---------------+\n");
+		printf("| Shared Memory ID     | %13d |\n", idSharedMemory);
+		printf("+----------------------+---------------+\n");
+		printf("| Process ID (PID)     | %13d |\n", getpid());
+		printf("+----------------------+---------------+\n");
+		*/
 	}
 
+	/*
 	printf("\n");
 	printf("+->->->->->->->->->->-+\n");
 	printf("$ PROSES BERLANGSUNG! $\n");
 	printf("+-<-<-<-<-<-<-<-<-<-<-+\n");
 	printf("\n");
+	*/
 	/*
 		Pada bagian ini, Simulator OS (OS.c) bekerja dengan menunggu sinyal SIGUSR1 dari program MMU (MMU.c)
-
-		kode sbb:
-
-			signal(SIGUSR1, penangananSignal);
-		    while (1)
-		        pause();
 	*/
 
+	// menangani interup ctrl-c dari keyboard
+	signal(SIGINT, penangananCtrlC);
+
+	//# 1, 2, 3, 4, 5, 6, 7, 8
+	signal(SIGUSR1, penangananSIGUSR1);
+
+	//# 9. Jika tidak ditemukan field Requested dengan nilai bukan nol, berhenti.
+	while(statusOS != -1)
+		pause();
+
+	// OS Selesai
+	printf("The MMU has finished\n");
+	PrintPageTable(dataPage, banyakPage);
+	printf("%d disk accesses required\n\n", aksesDisk);
+
+
 	if(dealokasiSharedMemory(idSharedMemory) == -1){
-		printf("Dealokasi Shared Memory Gagal\n");
+		//printf("Dealokasi Shared Memory Gagal\n");
 	}else{
-		printf("Dealokasi Shared Memory Berhasil\n");
+		//printf("Dealokasi Shared Memory Berhasil\n");
 	}
 
-	printf("OS Selesai\n");
+	//printf("OS Selesai\n");
 
 	return 0;
 }
 
-
-/** LINUX Standard signals
-
-	Linux supports the standard signals listed below.  Several signal
-	numbers are architecture-dependent, as indicated in the "Value"
-	column.  (Where three values are given, the first one is usually
-	valid for alpha and sparc, the middle one for x86, arm, and most
-	other architectures, and the last one for mips.  (Values for parisc
-	are not shown; see the Linux kernel source for signal numbering on
-	that architecture.)  A - denotes that a signal is absent on the
-	corresponding architecture.)
-
-	First the signals described in the original POSIX.1-1990 standard.
-
-	Signal     Value     Action   Comment
-	──────────────────────────────────────────────────────────────────────
-	SIGHUP        1       Term    Hangup detected on controlling terminal
-	                              or death of controlling process
-	SIGINT        2       Term    Interrupt from keyboard
-	SIGQUIT       3       Core    Quit from keyboard
-	SIGILL        4       Core    Illegal Instruction
-	SIGABRT       6       Core    Abort signal from abort(3)
-	SIGFPE        8       Core    Floating point exception
-	SIGKILL       9       Term    Kill signal
-	SIGSEGV      11       Core    Invalid memory reference
-	SIGPIPE      13       Term    Broken pipe: write to pipe with no readers
-	SIGALRM      14       Term    Timer signal from alarm(2)
-	SIGTERM      15       Term    Termination signal
-	SIGUSR1   30,10,16    Term    User-defined signal 1
-	SIGUSR2   31,12,17    Term    User-defined signal 2
-	SIGCHLD   20,17,18    Ign     Child stopped or terminated
-	SIGCONT   19,18,25    Cont    Continue if stopped
-	SIGSTOP   17,19,23    Stop    Stop process
-	SIGTSTP   18,20,24    Stop    Stop typed at terminal
-	SIGTTIN   21,21,26    Stop    Terminal input for background process
-	SIGTTOU   22,22,27    Stop    Terminal output for background process
-
-	The signals SIGKILL and SIGSTOP cannot be caught, blocked, or
-	ignored.
-
-	Next the signals not in the POSIX.1-1990 standard but described in
-	SUSv2 and POSIX.1-2001.
-
-	Signal       Value     Action   Comment
-	────────────────────────────────────────────────────────────────────
-	SIGBUS      10,7,10     Core    Bus error (bad memory access)
-	SIGPOLL                 Term    Pollable event (Sys V).
-	                                Synonym for SIGIO
-	SIGPROF     27,27,29    Term    Profiling timer expired
-	SIGSYS      12,31,12    Core    Bad argument to routine (SVr4)
-	SIGTRAP        5        Core    Trace/breakpoint trap
-	SIGURG      16,23,21    Ign     Urgent condition on socket (4.2BSD)
-	SIGVTALRM   26,26,28    Term    Virtual alarm clock (4.2BSD)
-	SIGXCPU     24,24,30    Core    CPU time limit exceeded (4.2BSD)
-	SIGXFSZ     25,25,31    Core    File size limit exceeded (4.2BSD)
-
-	Up to and including Linux 2.2, the default behavior for SIGSYS,
-	SIGXCPU, SIGXFSZ, and (on architectures other than SPARC and MIPS)
-	SIGBUS was to terminate the process (without a core dump).  (On some
-	other UNIX systems the default action for SIGXCPU and SIGXFSZ is to
-	terminate the process without a core dump.)  Linux 2.4 conforms to
-	the POSIX.1-2001 requirements for these signals, terminating the
-	process with a core dump.
-
-	Next various other signals.
-
-	Signal       Value     Action   Comment
-	────────────────────────────────────────────────────────────────────
-	SIGIOT         6        Core    IOT trap. A synonym for SIGABRT
-	SIGEMT       7,-,7      Term
-	SIGSTKFLT    -,16,-     Term    Stack fault on coprocessor (unused)
-	SIGIO       23,29,22    Term    I/O now possible (4.2BSD)
-	SIGCLD       -,-,18     Ign     A synonym for SIGCHLD
-	SIGPWR      29,30,19    Term    Power failure (System V)
-	SIGINFO      29,-,-             A synonym for SIGPWR
-	SIGLOST      -,-,-      Term    File lock lost (unused)
-	SIGWINCH    28,28,20    Ign     Window resize signal (4.3BSD, Sun)
-	SIGUNUSED    -,31,-     Core    Synonymous with SIGSYS
-
-	(Signal 29 is SIGINFO / SIGPWR on an alpha but SIGLOST on a sparc.)
-
-	SIGEMT is not specified in POSIX.1-2001, but nevertheless appears on
-	most other UNIX systems, where its default action is typically to
-	terminate the process with a core dump.
-
-	SIGPWR (which is not specified in POSIX.1-2001) is typically ignored
-	by default on those other UNIX systems where it appears.
-
-	SIGIO (which is not specified in POSIX.1-2001) is ignored by default
-	on several other UNIX systems.
-
-	Where defined, SIGUNUSED is synonymous with SIGSYS on most
-	architectures.
-
-**/
